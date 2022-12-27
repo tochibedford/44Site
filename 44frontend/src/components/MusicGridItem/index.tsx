d2a8cad4aa3bf40e09ed536f0f92181af979338d
johnwav <102820657+johnwav@users.noreply.github.com>
2022-12-27 11:10:18 +0100

    Feat: Music grid item, receives meta data as props

---
+++ 44frontend/src/components/MusicGridItem/index.tsx
@@ -0,0 +1,26 @@
+import styles from "./MusicGridItem.module.scss";
+
+interface MusicGridItemProps {
+  title: string;
+  artist: string;
+  features: string;
+  album_art: string;
+}
+
+export default function MusicGridItem({
+  title,
+  artist,
+  features,
+  album_art,
+}: MusicGridItemProps) {
+  return (
+    <div className={styles.item}>
+      <img className={styles.cover} src={album_art} alt="albumart" />
+
+      {/* meta data will be used in expanded view of grid item */}
+      {/* <h1>{title}</h1>
+      <h1>{artist}</h1>
+      <h1>{features}</h1> */}
+    </div>
+  );
+}
--- 44frontend/src/pages/TestPage/JohnTests/index.tsx
+++ 44frontend/src/pages/TestPage/JohnTests/index.tsx
@@ -1,7 +1,11 @@
 import ComponentGroup from "../ComponentGroup"
 import NavButton from "../../../components/NavButton/NavButton"
+import MusicGridItem from "../../../components/MusicGridItem"
 
 export default function JohnTests() {
+    
+    const MusicGridObject = {title: "Rather Be", artist: "Saint Austin", features: "Saint, John & Tochi", album_art:'https://www.shutterstock.com/image-vector/vertical-seamless-pattern-zodiac-signs-600w-416670184.jpg'} ;
+
     return (
         <>
             <ComponentGroup author="John" componentName="Button">
@@ -15,6 +19,11 @@ export default function JohnTests() {
                 <NavButton type="email" text="Contact" />
                 <NavButton type="link" to="/feed" text="Feed" />
             </ComponentGroup>
+            <ComponentGroup author="John" componentName="MusicGridItem">
+               <MusicGridItem  {...MusicGridObject} />
+            </ComponentGroup>
+
+
         </>
     )
 }
