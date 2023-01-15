cf466b0b375d43a280cecd35e1ef954aded21f13
tochibedford <musicofbyte@gmail.com>
2023-01-16 00:44:18 +0100

    feat: implemented basic discography panel

--- 44frontend/src/components/Panel/index.tsx
+++ 44frontend/src/components/Panel/index.tsx
@@ -1,29 +1,64 @@
 import styles from "./Panel.module.scss"
 import arrow from "./images/arrow_blue.svg"
+import { Link } from "react-router-dom"
 
-interface IPanel {
+interface IPanelBase {
     text: string;
     last?: boolean;
     flipped?: boolean;
     noInfo?: boolean;
 }
 
-export default function Panel({ text, last = false, flipped = false, noInfo = false }: IPanel) {
-    return (
-        <div className={`${styles.container} ${last ? styles.last__panel : ""} ${flipped ? styles.flipped : ""}`}>
-            <div className={styles.text__content}>{text}</div>
-            <div className={styles.divider} />
-            <div className={styles.panel__action__detail}>
-                {!noInfo ? (
-                    <div className={styles.panel__detail__container}>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque ad ex deleniti ratione a nam tempore dolorem tempora rem voluptatum!
-                    </div>) :
-                    (<div className={styles.arrow__container}>
-                        <img src={arrow} alt="arrow icon" className={flipped ? styles.image__flipped : ""} />
-                    </div>)
-                }
+type IPanelConditional = {
+    action: "link";
+    url: string
+    setIsOpen?: never;
+} | {
+    action: "button";
+    setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+    url?: never
+}
 
+type IPanel = IPanelBase & IPanelConditional
+
+export default function Panel({ text, last = false, flipped = false, noInfo = false, action = "link", url, setIsOpen }: IPanel) {
+    if (action === "link" && url) { // panel acts as a link
+        return (
+            <Link to={url} className={`${styles.container} ${last ? styles.last__panel : ""} ${flipped ? styles.flipped : ""}`}>
+                <div className={styles.text__content}>{text}</div>
+                <div className={styles.divider} />
+                <div className={styles.panel__action__detail}>
+                    {!noInfo ? (
+                        <div className={styles.panel__detail__container}>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque ad ex deleniti ratione a nam tempore dolorem tempora rem voluptatum!
+                        </div>) :
+                        (<div className={styles.arrow__container}>
+                            <img src={arrow} alt="arrow icon" className={flipped ? styles.image__flipped : ""} />
+                        </div>)
+                    }
+
+                </div>
+            </Link>
+        )
+    } else {
+        return (
+            <div className={`${styles.container} ${last ? styles.last__panel : ""} ${flipped ? styles.flipped : ""}`} onClick={() => { setIsOpen && setIsOpen(true) }}>
+                <input type="checkbox" className={styles.panel__check} />
+                <div className={styles.text__content}>{text}</div>
+                <div className={styles.divider} />
+                <div className={styles.panel__action__detail}>
+                    {!noInfo ? (
+                        <div className={styles.panel__detail__container}>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque ad ex deleniti ratione a nam tempore dolorem tempora rem voluptatum!
+                        </div>) :
+                        (<div className={styles.arrow__container}>
+                            <img src={arrow} alt="arrow icon" className={flipped ? styles.image__flipped : ""} />
+                        </div>)
+                    }
+
+                </div>
             </div>
-        </div>
-    )
+        )
+
+    }
 }
--- 44frontend/src/pages/TestPage/TochiTests/index.tsx
+++ 44frontend/src/pages/TestPage/TochiTests/index.tsx
@@ -1,25 +1,36 @@
 import ComponentGroup from "../ComponentGroup"
 import Panel from "../../../components/Panel"
 import MusicCard from "../../Home/MusicCard"
+import DiscographyPanel from "../../Home/DiscographyPanel"
+import { useState } from "react"
 
 export default function TochiTests() {
+    const [isDiscographyOpen, setIsDiscographyOpen] = useState(false);
     return (
         <>
             <ComponentGroup author="Tochi" componentName="Panel">
                 {(["Full Discography", "JOHN WAV", "TOCHI BEDFORD"]).map((item, index, array) => {
-                    return (
-                        <Panel key={index} text={item} flipped={!(index % 2 === 0)} last={index === array.length - 1} noInfo={item === "Full Discography"} />
-                    )
+
+                    if (item === "Full Discography") {
+                        return (
+                            <Panel key={index} text={item} flipped={!(index % 2 === 0)} last={index === array.length - 1} noInfo={item === "Full Discography"} action="link" url="/discography" />
+                        )
+                    } else {
+                        return (
+                            <Panel key={index} text={item} flipped={!(index % 2 === 0)} last={index === array.length - 1} action="button" setIsOpen={setIsDiscographyOpen} />
+                        )
+                    }
                 })}
             </ComponentGroup>
 
             <ComponentGroup author="Tochi" componentName="Music Card">
-                <MusicCard />
+                < MusicCard />
             </ComponentGroup>
 
             <ComponentGroup author="Tochi" componentName="Discography on click">
                 <>Click the panel to bring out the discography pane</>
-                <Panel text={"John Wav"} last={true} noInfo={true} />
+                <Panel text={"John Wav"} last={true} noInfo={true} action="button" setIsOpen={setIsDiscographyOpen} />
+                <DiscographyPanel isOpen={isDiscographyOpen} setIsOpen={setIsDiscographyOpen} />
             </ComponentGroup>
         </>
     )
