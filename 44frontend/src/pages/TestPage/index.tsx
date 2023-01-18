0c4242dcb030d08f3902c3d7d2514bc9ff2a51df
Martins Ikpe <martins.ikpe@stu.cu.edu.ng>
2023-01-18 10:24:20 +0100

    feat: Added NavbarLayout Component

--- 44frontend/src/pages/TestPage/index.tsx
+++ 44frontend/src/pages/TestPage/index.tsx
@@ -3,19 +3,22 @@ import styles from "./TestPage.module.scss"
 import JohnTests from "./JohnTests"
 import TochiTests from "./TochiTests"
 import MartieTests from "./MartieTests"
+import Layout from "../../components/NavLayout"
 
 export default function TestPage() {
     return (
-        <div className={styles.bodyContainer}>
-            <div className={styles.body}>
-                <h1>
-                    This is the test Route, Place components here to check them out visually : )
-                </h1>
-                {/* Juts wrap your component in this "ComponentGroup Component to separate it into it's own box" */}
-                <TochiTests />
-                <JohnTests />
-                <MartieTests />
+        <Layout>
+            <div className={styles.bodyContainer}>
+                <div className={styles.body}>
+                    <h1>
+                        This is the test Route, Place components here to check them out visually : )
+                    </h1>
+                    {/* Juts wrap your component in this "ComponentGroup Component to separate it into it's own box" */}
+                    <TochiTests />
+                    <JohnTests />
+                    <MartieTests />
+                </div>
             </div>
-        </div>
+        </Layout>
     )
 }
