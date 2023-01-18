afc8dabce6922da6a80e987a947d52274532bea1
tochibedford <musicofbyte@gmail.com>
2023-01-19 00:12:10 +0100

    feat: added hero section to the Home page

--- 44frontend/src/pages/Home/index.tsx
+++ 44frontend/src/pages/Home/index.tsx
@@ -1,9 +1,16 @@
-import styles from "Home.module.scss"
-import NavButton from "../../components/NavButton/NavButton"
-
+import styles from "./Home.module.scss"
+import CardCarousel from "./CardCarousel"
 
 export default function Home() {
     return (
-        <div>44site</div>
+        <>
+            <section className={styles.hero}>
+                <div className={styles.page__info}>
+                    <h1 className={styles.title}>44DB</h1>
+                    <h2 className={styles.subtitle}>The Super Producer Collective Redefining The Music Industry</h2>
+                </div>
+                <div className={styles.carousel__container}><CardCarousel /></div>
+            </section>
+        </>
     )
 }
