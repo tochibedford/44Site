a8becddefb79ae6aa49f67da07a2fc6927686bfe
tochibedford <musicofbyte@gmail.com>
2023-01-18 22:32:49 +0100

    chore: use the NavLayout as a general wrapper

--- 44frontend/src/App.tsx
+++ 44frontend/src/App.tsx
@@ -3,17 +3,18 @@ import Home from './pages/Home';
 import Feed from './pages/Feed';
 import Discography from './pages/Discography';
 import TestPage from './pages/TestPage';
+import NavLayout from './components/NavLayout';
 
 function App() {
   return (
-    <>
+    <NavLayout>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/feed" element={<Feed />} />
         <Route path="/disco" element={<Discography />} />
         <Route path="/test" element={<TestPage />} />
       </Routes>
-    </>
+    </NavLayout>
   )
 }
 
