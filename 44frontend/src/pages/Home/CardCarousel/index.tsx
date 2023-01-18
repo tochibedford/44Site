8f92f8d72aa8697ee96eb796d641acd0c3bca5ae
tochibedford <musicofbyte@gmail.com>
2023-01-18 22:49:30 +0100

    fix: set a max width for card carousel

--- 44frontend/src/pages/Home/CardCarousel/index.tsx
+++ 44frontend/src/pages/Home/CardCarousel/index.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useEffect, useState } from "react"
+import { useEffect, useState } from "react"
 import MusicCard from "../MusicCard"
 import styles from "./CardCarousel.module.scss"
 
