100a7ab9148f5ed18f271a6048420eafe48abd87
Martins Ikpe <martins.ikpe@stu.cu.edu.ng>
2023-01-18 18:39:57 +0100

    fix: current time not updating properly

--- 44frontend/src/components/Navbar/index.tsx
+++ 44frontend/src/components/Navbar/index.tsx
@@ -1,7 +1,7 @@
 import styles from "./Navbar.module.scss"
 import NavButton from "../NavButton/NavButton"
 import Logo from "./icons/Logo"
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Navbar() {
     const currentDay = new Date()
@@ -10,12 +10,15 @@ export default function Navbar() {
             month: "short"
         }).toLocaleUpperCase()
 
-    const currentTime = new Date()
-        .toLocaleTimeString([], {
-            hour: '2-digit',
-            minute: '2-digit',
-            hour12: true
-        }).toLocaleUpperCase()
+    const [currentTime, setCurrentTime] = useState("")
+    useEffect(() => {
+        const cTime = new Date();
+        const timer = setInterval(() => {
+            setCurrentTime(`${cTime.toLocaleTimeString([], { hour12: true }).toLocaleUpperCase()}`)
+        }, 1000)
+        return () => clearInterval(timer);
+    })
+
     return (
         <div className={styles.navbarWrapper}>
             <div className={styles.logo}>
