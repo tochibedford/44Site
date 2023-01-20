1182b7f6c72669336f34404a8549db478a640e44
tochibedford <musicofbyte@gmail.com>
2023-01-20 20:46:48 +0100

    feat: work schema to support array of references

--- 44sanity/schemas/work.ts
+++ 44sanity/schemas/work.ts
@@ -7,20 +7,23 @@ export default {
             name: 'title',
             type: 'string',
             title: 'Title',
-            description: 'Title of this work'
+            description: 'Title of this work',
+            validation: (Rule: any) => Rule.required()
         },
         {
             name: 'contributor',
-            type: 'reference',
+            type: 'array',
             title: 'Contributor',
-            to: [{ type: 'talent' }],
-            description: 'Name of the contributor in 44 that was involved in this work'
+            of: [{ type: 'reference', to: [{ type: 'talent' }] }],
+            description: 'Name of the contributor in 44 that was involved in this work',
+            validation: (Rule: any) => Rule.required()
         },
         {
             name: 'artistName',
             type: 'string',
             title: 'Artist Name',
-            description: 'Name of the artist on the song/instrumental as seen on streaming services'
+            description: 'Name of the artist on the song/instrumental as seen on streaming services',
+            validation: (Rule: any) => Rule.required()
         },
         {
             name: 'features',
@@ -33,7 +36,8 @@ export default {
             name: 'cover',
             type: 'image',
             title: 'Cover Art',
-            desctription: 'Cover Art for this work'
+            desctription: 'Cover Art for this work',
+            validation: (Rule: any) => Rule.required()
         }
     ]
 }
