b7fe42bac36dbb8e7b46edd810c84ce86db37461
tochibedford <musicofbyte@gmail.com>
2023-03-27 11:45:58 +0100

    feat: added token to environment variables

--- 44frontend/src/client.ts
+++ 44frontend/src/client.ts
@@ -3,6 +3,7 @@ import client from "@sanity/client"
 export default client({
     projectId: "10mrxkqq",
     dataset: "production",
-    useCdn: true,
+    token: import.meta.env.VITE_SANITY_TOKEN,
+    useCdn: false,
     apiVersion: "2023-01-20"
 })
