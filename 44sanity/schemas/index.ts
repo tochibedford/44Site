c44655677b6769db7a1174d58520f44d725413f1
tochibedford <musicofbyte@gmail.com>
2023-03-28 07:07:50 +0100

    feat: removed user schema

--- 44sanity/schemas/index.ts
+++ 44sanity/schemas/index.ts
@@ -2,6 +2,5 @@ import talent from "./talent"
 import feed from "./feed"
 import work from "./work"
 import dsp from "./dsp"
-import user from "./user"
 
-export const schemaTypes = [talent, feed, work, dsp, user]
+export const schemaTypes = [talent, feed, work, dsp]
